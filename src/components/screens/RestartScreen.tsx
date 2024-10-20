eec075f3bc98ade5e16e41a52c64296c3047cd76
Yukine213 <kondratenkov1109@gmail.com>
2024-10-20 19:18:47 +0300

    add a restart functionality

--- src/components/screens/RestartScreen.tsx
+++ src/components/screens/RestartScreen.tsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import {
+  initiatePowerOnSequence,
+  setShowLoadingScreen,
+  setShowRestartScreen,
+} from "../../redux/slices/appSlice";
+import Loading from "../Loading";
+
 const RestartScreen = () => {
-  return <div>RestartScreen</div>;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const restartTimeout = setTimeout(() => {
+      dispatch(setShowRestartScreen(false));
+      dispatch(setShowLoadingScreen(true));
+
+      const powerOnTimeout = setTimeout(() => {
+        dispatch(initiatePowerOnSequence());
+      }, 2500);
+
+      return () => clearTimeout(powerOnTimeout);
+    }, 2500);
+
+    return () => clearTimeout(restartTimeout);
+  }, [dispatch]);
+
+  return (
+    <div className="bg-black flex justify-center items-center h-screen flex-col">
+      <Loading message="Restarting..." />
+    </div>
+  );
 };
 
 export default RestartScreen;
