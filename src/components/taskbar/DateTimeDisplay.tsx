f1afc65690ee1500b7292944d35ad0fc91d8431a
Yukine213 <kondratenkov1109@gmail.com>
2024-10-31 18:20:09 +0200

    feat: implement useEffect for real-time clock updates in DateTimeDisplay

--- src/components/taskbar/DateTimeDisplay.tsx
+++ src/components/taskbar/DateTimeDisplay.tsx
@@ -1,21 +1,37 @@
+import { useEffect, useState } from "react";
+
 const DateTimeDisplay = () => {
-  const date = new Date();
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  const formatDate = (date: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    };
+    return date.toLocaleDateString([], options).replace(/^0/, "");
+  };
+
+  const formatTime = (time: Date) => {
+    const options: Intl.DateTimeFormatOptions = {
+      hour: "numeric",
+      minute: "2-digit",
+    };
+    return time.toLocaleTimeString([], options).replace(/^0/, "");
+  };
 
   return (
     <div className="text-center hover:bg-[#272727] px-3 transition-colors duration-100 text-sm ">
-      <p>
-        {(date.getHours() % 12 || 12) +
-          ":" +
-          date.getMinutes().toString().padStart(2, "0") +
-          (date.getHours() >= 12 ? " PM" : " AM")}
-      </p>
-      <p>
-        {(date.getMonth() + 1).toString().padStart(2, "0") +
-          "/" +
-          date.getDate().toString().padStart(2, "0") +
-          "/" +
-          date.getFullYear().toString()}
-      </p>
+      <p>{formatTime(currentTime)}</p>
+      <p>{formatDate(currentTime)}</p>
     </div>
   );
 };
