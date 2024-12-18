718519533312d7545a997dd9bc3f4a75b46dc0bd
Yukine213 <kondratenkov1109@gmail.com>
2024-12-18 20:42:35 +0200

    refactor:move screen rendering logic to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import PowerOnScreen from "./components/screens/PowerOnScreen";
 import Taskbar from "./components/taskbar/Taskbar";
-
-import SleepScreen from "./components/screens/SleepScreen";
-import RestartScreen from "./components/screens/RestartScreen";
-import ShutDownScreen from "./components/screens/ShutDownScreen";
 import Apps from "./components/apps/Apps";
 import Calculator from "./components/apps/Calculator";
 import Settings from "./components/settings/Settings";
 import ContextMenu from "./components/context-menu/ContextMenu";
 import useContextMenuLogicHook from "./hooks/useContextMenuLogicHook";
 import useAppLogicHook from "./hooks/useAppLogicHook";
+import ScreenRenderer from "./components/screens/ScreenRenderer";
 
 const App = () => {
   const {
     constraintRef,
     showApp,
-    showLoadingScreen,
-    showSleepScreen,
-    showRestartScreen,
-    showShutDownScreen,
+
     openedApps,
     minimizedApps,
     wallpaper,
@@ -28,10 +21,7 @@ const App = () => {
 
   return (
     <main onContextMenu={handleRightClick}>
-      {showLoadingScreen && <PowerOnScreen />}
-      {showSleepScreen && <SleepScreen />}
-      {showRestartScreen && <RestartScreen />}
-      {showShutDownScreen && <ShutDownScreen />}
+      <ScreenRenderer /> {/* Render the screen based on the state  */}
       {showApp && (
         <div ref={constraintRef} className="relative  w-[100vw] h-[100vh]">
           <img
---
+++ src/components/screens/ScreenRenderer.tsx
@@ -0,0 +1,21 @@
+import PowerOnScreen from "./PowerOnScreen";
+import SleepScreen from "./SleepScreen";
+import RestartScreen from "./RestartScreen";
+import ShutDownScreen from "./ShutDownScreen";
+import useAppLogicHook from "../../hooks/useAppLogicHook";
+
+const ScreenRenderer = () => {
+  const {
+    showLoadingScreen,
+    showSleepScreen,
+    showRestartScreen,
+    showShutDownScreen,
+  } = useAppLogicHook();
+  if (showLoadingScreen) return <PowerOnScreen />;
+  if (showSleepScreen) return <SleepScreen />;
+  if (showRestartScreen) return <RestartScreen />;
+  if (showShutDownScreen) return <ShutDownScreen />;
+  return null;
+};
+
+export default ScreenRenderer;
