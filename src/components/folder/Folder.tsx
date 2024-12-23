7ba1170973b03ca003267d6046f2567f529baacc
Yukine213 <kondratenkov1109@gmail.com>
2024-12-23 21:00:18 +0200

    fix css

--- src/components/folder/Folder.tsx
+++ src/components/folder/Folder.tsx
@@ -4,19 +4,19 @@ const Folder = () => {
   const { folders } = useAppSelector((state) => state.folders);
 
   return (
-    <div>
-      <div className="absolute mt-2  px-2 py-16">
+    <div className="relative p-1 h-full w-full">
+      <div className="flex  flex-wrap items-start gap-1 ">
+        {" "}
         {folders.map((folder, index) => (
           <div
             key={index}
-            className="flex flex-col items-center mb-2 cursor-pointer text-white "
+            className="first:ml-16 flex flex-col items-center text-white cursor-pointer"
+            style={{ width: "80px" }}
           >
-            <img
-              src="folder.png"
-              alt="Folder icon"
-              className="size-10 w-1/2 mr-2  "
-            />
-            <span className="text-[13px]">{folder}</span>
+            <img src="folder.png" alt="Folder icon" className="size-10" />
+            <span className="text-[12px] text-center  truncate w-full">
+              {folder}
+            </span>
           </div>
         ))}
       </div>
