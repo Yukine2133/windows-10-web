19d23c7f960be4455f9cbb8847efce97e5550152
Yukine213 <kondratenkov1109@gmail.com>
2025-01-03 14:07:32 +0200

    feat: change a text size and width of the div

--- src/components/context-menu/DesktopItem.tsx
+++ src/components/context-menu/DesktopItem.tsx
@@ -14,11 +14,11 @@ const DesktopItem = ({ name, icon, handleRightClick }: DesktopItemProps) => {
         e.stopPropagation();
         handleRightClick(e, { name, type: "Item" });
       }}
-      className="first:ml-16 flex flex-col items-center text-white cursor-pointer"
-      style={{ width: "80px" }}
+      className="first:ml-16 flex flex-col  items-center text-white cursor-pointer"
+      style={{ width: "70px" }}
     >
       <img src={icon} alt="Item icon" className="size-10" />
-      <span className="text-[12px] text-center truncate w-full">{name}</span>
+      <span className="text-[11.5px] text-center  w-full">{name}</span>
     </div>
   );
 };
