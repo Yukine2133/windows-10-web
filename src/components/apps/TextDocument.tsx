5203d1f61625d481f8ec6ca7972b70ecb2eb9d82
Yukine213 <kondratenkov1109@gmail.com>
2025-01-08 15:23:59 +0200

    finish text document ui

--- src/components/apps/TextDocument.tsx
+++ src/components/apps/TextDocument.tsx
@@ -1,4 +1,8 @@
 import { motion } from "framer-motion";
+import WindowControls from "../WindowControls";
+import { useAppDispatch } from "../../hooks/reduxHooks";
+import { closeApp, minimizeApp } from "../../redux/slices/appSlice";
+import { useState } from "react";
 
 const TextDocument = ({
   constraintRef,
@@ -7,18 +11,35 @@ const TextDocument = ({
   constraintRef: React.MutableRefObject<null>;
   name: string;
 }) => {
+  const dispatch = useAppDispatch();
+
+  const [isDragging, setIsDragging] = useState(true);
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
-      drag
+      drag={isDragging}
       dragConstraints={constraintRef}
       dragMomentum={false}
-      className="absolute w-[20rem] text-white bg-[#202020] left-[42%] top-[21%] "
+      className={`absolute w-[60rem] z-10 h-[20rem] text-white bg-[#202020] left-[calc(50%-30rem)] top-[21%] `}
     >
-      <header className="p-2 bg-gray-700">{name}</header>
-      <div className="p-4">TextDocument Content</div>
+      <div className="flex justify-between items-center  border-b border-gray-700">
+        <h3 className=" py-2 px-4">{name}</h3>
+        <WindowControls
+          minimizeApp={() => dispatch(minimizeApp("TextDocument"))}
+          closeApp={() => dispatch(closeApp({ type: "TextDocument" }))}
+        />
+      </div>
+
+      <div className="">
+        <textarea
+          onMouseEnter={() => setIsDragging(false)}
+          onMouseLeave={() => setIsDragging(true)}
+          className="w-full resize-none outline-none h-[20rem]  p-2 bg-[#202020]  text-lg  text-white"
+          autoFocus
+        />
+      </div>
     </motion.div>
   );
 };
