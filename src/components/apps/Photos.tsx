c3ae737cff4421886cf2e4f76891639d9827122c
Yukine213 <kondratenkov1109@gmail.com>
2025-01-14 14:55:19 +0200

    feat: finish PhotosBottomBar component

--- src/components/apps/Photos.tsx
+++ src/components/apps/Photos.tsx
@@ -1,8 +1,23 @@
-import { BiEdit, BiPrinter, BiTrash } from "react-icons/bi";
+import {
+  BiEdit,
+  BiHeart,
+  BiPrinter,
+  BiSave,
+  BiTrash,
+  BiZoomIn,
+  BiZoomOut,
+} from "react-icons/bi";
 import AppWindow from "./AppWindow";
 import { GrRotateRight } from "react-icons/gr";
 import { FaRegShareSquare } from "react-icons/fa";
 import { BsThreeDots } from "react-icons/bs";
+import { PiFilmStripLight } from "react-icons/pi";
+import { CiCircleInfo } from "react-icons/ci";
+import { RxDimensions } from "react-icons/rx";
+import { TbWorldSearch } from "react-icons/tb";
+import { MdOutlineZoomOutMap } from "react-icons/md";
+import { useState } from "react";
+import { LuMoveDiagonal } from "react-icons/lu";
 
 const Photos = ({
   constraintRef,
@@ -11,17 +26,20 @@ const Photos = ({
   constraintRef: React.MutableRefObject<null>;
   name: string;
 }) => {
+  const [isDragging, setIsDragging] = useState(true);
   return (
     <AppWindow
       title={name}
       type="Photos"
+      drag={isDragging}
       constraintRef={constraintRef}
-      className="top-[1rem] left-[calc(41.7%-30rem)]  w-[65rem] h-[75%] shadow-lg"
+      className="top-[1rem] left-[calc(41.7%-30rem)]  w-[65rem] h-[520px] shadow-2xl"
       windowControlsClassName="border-b border-gray-700"
     >
       <div className="flex justify-center h-[440px]  items-center">
-        <img src={name} alt={name} className="w-3/4 h-full object-cover" />
+        <img src={name} alt={name} className="w-3/4 h-full object-contain" />
       </div>
+      <PhotosBottomBar setIsDragging={setIsDragging} />
     </AppWindow>
   );
 };
@@ -30,7 +48,7 @@ export default Photos;
 
 export const PhotosTopBar = ({ name }: { name: string }) => {
   return (
-    <div className="px-4 flex items-center justify-center ">
+    <div className="px-2 flex items-center justify-center ">
       <div className="flex items-center ">
         <div className="flex bg-[#f783b2] p-2 rounded-lg text-black items-center gap-1 mr-2 cursor-pointer">
           <BiEdit className="size-6" />
@@ -65,6 +83,58 @@ export const PhotosTopBar = ({ name }: { name: string }) => {
             alt="Onedrive icon"
             className="size-9 hover:bg-[#2d2d2d] p-2 rounded-lg cursor-pointer "
           />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const PhotosBottomBar = ({
+  setIsDragging,
+}: {
+  setIsDragging: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <div className="px-2 pt-1 flex items-center justify-between shadow-lg  ">
+      <div className="flex items-center gap-4 opacity-50 ">
+        <div className="pr-2 border-r border-gray-700">
+          <PiFilmStripLight className="size-5  " />
+        </div>
+        <BiHeart className="size-5 " />
+        <div className="pr-2 border-r border-gray-700">
+          <CiCircleInfo className="size-5 " />
+        </div>
+        <div className="flex items-center gap-2  text-sm">
+          <RxDimensions className="size-4 " />
+          <h2>3840 x 2160</h2>
+        </div>
+        <div className="flex items-center gap-2  text-sm">
+          <BiSave className="size-5 " />
+          <h2>1.9 MB</h2>
+        </div>
+      </div>
+
+      <TbWorldSearch className="size-5 " />
+
+      <div className="flex items-center gap-4">
+        <MdOutlineZoomOutMap className="size-5 " />
+
+        <div className=" flex gap-3  items-center">
+          <BiZoomOut className="size-5" />
+          <input
+            type="range"
+            name=""
+            id=""
+            onMouseEnter={() => setIsDragging(false)}
+            onMouseLeave={() => setIsDragging(true)}
+            className="w-24 h-1 cursor-pointer bg-white bg-opacity-80 rounded-full appearance-none outline-none 
+  accent-[#f783b2] hover:accent-[#ff99c9]"
+          />
+          <BiZoomIn className="size-5" />
+        </div>
+
+        <div className="border-l border-gray-700 pl-2">
+          <LuMoveDiagonal className="size-6" />
         </div>
       </div>
     </div>
--- src/components/apps/Pictures.tsx
+++ src/components/apps/Pictures.tsx
@@ -16,19 +16,19 @@ const Pictures = ({
       className="top-[2rem] left-[calc(41.7%-30rem)]  w-[75rem] h-[75%]"
       windowControlsClassName="border-b border-gray-700"
     >
-      <div className="flex items-center gap-6 p-2">
+      <div className="flex items-center gap-4 p-2">
         {pictures.map((picture) => (
           <div
             onClick={() => toggleApp({ type: "Photos", name: picture.img })}
             key={picture.id}
-            className="cursor-pointer hover:bg-[#4d4d4d] p-2 pt-8"
+            className="cursor-pointer  hover:bg-[#4d4d4d] p-2 pt-8"
           >
             <img
               src={picture.img}
-              alt={picture.label}
-              className=" w-32 object-cover "
+              alt={picture.img}
+              className="mx-auto w-32 h-20 object-contain "
             />
-            <h1 className="text-sm text-center">{picture.label}</h1>
+            <h1 className="text-sm text-center">{picture.img}</h1>
           </div>
         ))}
       </div>
