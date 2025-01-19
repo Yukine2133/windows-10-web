ebc1f13ae6209f730c76b1e1f17c7d16e9675f1a
Yukine213 <kondratenkov1109@gmail.com>
2025-01-19 15:37:21 +0200

    improve the loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,51 +1,39 @@
-const Loading = ({ message }: { message: string }) => {
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 120 120"
-        width="80px"
-        height="80px"
-        preserveAspectRatio="xMidYMid"
-        className="lds-spinner"
-        style={{ background: "none" }}
-      >
-        {[...Array(8)].map((_, index) => {
-          const angle = index * 45;
-          const delay = index / 8 + "s";
+import "./../loading.css";
 
-          return (
-            <g transform={`rotate(${angle} 60 60)`} key={index}>
-              <circle cx="60" cy="25" r="7" fill="#585858">
-                {" "}
-                <animate
-                  attributeName="r"
-                  from="7"
-                  to="7"
-                  begin={delay}
-                  dur="1s"
-                  repeatCount="indefinite"
-                  keyTimes="0;1"
-                  values="5;0"
-                />
-                <animate
-                  attributeName="fill-opacity"
-                  from="1"
-                  to="1"
-                  begin={delay}
-                  dur="1s"
-                  repeatCount="indefinite"
-                  keyTimes="0;1"
-                  values="1;0"
-                />
-              </circle>
-            </g>
-          );
-        })}
-      </svg>
-      <p className="text-white text-lg">{message}</p>
+function Loading({ message }: { message: string }) {
+  return (
+    <div className="loadingCircle">
+      <div
+        style={
+          {
+            "--size": "64px",
+            "--dot-size": "6px",
+            "--dot-count": 6,
+            "--color": "#fff",
+            "--speed": "1s",
+            "--spread": "60deg",
+          } as React.CSSProperties
+        }
+        className="dots"
+      >
+        <div style={{ "--i": 0 } as React.CSSProperties} className="dot"></div>
+        <div style={{ "--i": 1 } as React.CSSProperties} className="dot"></div>
+        <div style={{ "--i": 2 } as React.CSSProperties} className="dot"></div>
+        <div style={{ "--i": 3 } as React.CSSProperties} className="dot"></div>
+        <div style={{ "--i": 4 } as React.CSSProperties} className="dot"></div>
+        <div style={{ "--i": 5 } as React.CSSProperties} className="dot"></div>
+      </div>
+      <div
+        style={{
+          color: "white",
+          marginTop: 10,
+          fontSize: 22,
+        }}
+      >
+        <p>{message}</p>
+      </div>
     </div>
   );
-};
+}
 
 export default Loading;
